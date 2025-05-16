3af52425add19f2e4ef0ebb1c62624b7030c9c2c
tajtanvvir1 <tanvirah1223@gmail.com>
2025-05-16 10:05:14 +0600

    Problem 5 Solve

--- Problems.ts
+++ Problems.ts
@@ -42,7 +42,13 @@ class Car extends Vehicle {
 }
 
 
-
+function processValue(value: string | number): number {
+    if (typeof value === "string") {
+        return value.length
+    } else {
+        return value * 2;
+    }
+}
 
 
 
