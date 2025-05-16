8a299c0f2c75d01f64a4a9db32a7637f693e292c
tajtanvvir1 <tanvirah1223@gmail.com>
2025-05-16 10:06:57 +0600

    Problem 8 Solve

--- Problems.ts
+++ Problems.ts
@@ -85,6 +85,17 @@ function getDayType(day: Day): string {
 }
 
 
+async function squareAsync(n: number): Promise<number> {
+    return new Promise<number>((resolve, reject) => {
+        if (n >= 0) {
+            setTimeout(() => {
+                resolve(n * n);
+            }, 1000);
+        } else {
+            reject("Error: Negative number not allowed");
+        }
+    });
+};
 
 
 
